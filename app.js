1c9d40716ad13f59998af6a83296b8f072a46cd4
anoj-kt <anojk28@gmail.com>
2022-08-08 14:47:21 +0200

    implemented MVC

--- app.js
+++ app.js
@@ -1,45 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const pokemonJson = require('./pokemon.json')
 const cors = require('cors');
+const pokemonRoutes = require('./routes/PokemonRoutes')
 
 app.use(cors({
     origin: '*'
 }))
 
-app.get('/', (req, res) => {
-    res.send('use /pokemon to access all pokemons')
-})
-
-app.get('/pokemon', (req, res) => {
-    res.send(pokemonJson)
-})
-
-app.get('/pokemon/:id', (req, res) => {
-    let pokeId = req.params.id;
-
-    if(pokeId > pokemonJson.length) {
-        res.status(404).send('404');
-    }
-
-    pokemonJson.map(x => {
-        if(x.id == pokeId){
-            res.send(x);
-        } 
-    })
-})
-
-app.get('/pokemon/:id/:info', (req, res) => {
-    let pokeId = req.params.id;
-    let info = req.params.info;
-
-    pokemonJson.map(x => {
-        if(x.id == pokeId){
-            res.send(x[info]);
-        }
-    })
-})
+app.use('/', pokemonRoutes);
 
 app.use((req, res) => {
     res.status(404).send('404');
