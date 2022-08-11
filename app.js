ef65feda50e6a0791fc1d813b675e85d98899707
anoj-kt <anojk28@gmail.com>
2022-08-11 10:57:47 +0200

    Established connection to database

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const app = express()
 const port = process.env.PORT || 3000
 const cors = require('cors');
 const pokemonRoutes = require('./routes/PokemonRoutes')
+const mongoose = require('mongoose');
+const { db } = require('./models/PokemonDatabase');
 
 app.use(cors({
     origin: '*'
 }))
+
+const dbURI = 'mongodb+srv://Pokefighters:Pokefighters1@pokefight.ziwnlry.mongodb.net/?retryWrites=true&w=majority';  
+mongoose.connect(dbURI)
+.then((result) => console.log('Connected to DB'))
+.catch((err) => console.log('error: ' + err))
 
 app.use('/', pokemonRoutes);
 
